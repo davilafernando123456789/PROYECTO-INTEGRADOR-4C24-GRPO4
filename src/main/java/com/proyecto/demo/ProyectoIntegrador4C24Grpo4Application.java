afe0d9aa135b8e957a31b52bd576ba38a2023858
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2023-05-15 23:48:09 -0500

    Primer commit

---
+++ src/main/java/com/proyecto/demo/ProyectoIntegrador4C24Grpo4Application.java
@@ -0,0 +1,13 @@
+package com.proyecto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegrador4C24Grpo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegrador4C24Grpo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/demo/ProyectoIntegrador4C24Grpo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegrador4C24Grpo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
